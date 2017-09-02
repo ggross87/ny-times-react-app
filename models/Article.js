d54f83dff1ac126a1489425e996c6d3b76db93fa
Germanual Gross <germanualgross@Germanuals-Air.attlocal.net>
2017-09-01 19:16:09 -0500

    new

---
+++ models/Article.js
@@ -0,0 +1,29 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: String
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+//Export model
+module.exports = Article;
